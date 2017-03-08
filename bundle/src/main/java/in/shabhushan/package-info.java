e993a77f5b537425f3cf14000b03074649186c40
Shashi Bhushan <sbhushan1@outlook.com>
2017-03-08 15:49:58 +0530

    Initial Commit

---
+++ bundle/src/main/java/in/shabhushan/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package in.shabhushan;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/in/shabhushan/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package in.shabhushan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
