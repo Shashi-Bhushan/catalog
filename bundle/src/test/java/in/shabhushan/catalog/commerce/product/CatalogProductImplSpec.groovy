83f249222aa2366b32d94419d74766bdab69fa53
Shashi Bhushan <sbhushan1@outlook.com>
2017-03-14 09:31:38 +0530

    Groovy Language Update

---
+++ bundle/src/test/java/in/shabhushan/catalog/commerce/product/CatalogProductImplSpec.groovy
@@ -0,0 +1,19 @@
+//package in.shabhushan.catalog.commerce.product
+//
+//import spock.lang.Specification
+//
+///**
+// * Created by Shashi Bhushan
+// *       on 14/3/17.
+// */
+//class CatalogProductImplSpec extends Specification {
+//    def "maximum of two numbers"() {
+//        expect:
+//        Math.max(a, b) == c
+//
+//        where:
+//        a << [3, 5, 9]
+//        b << [7, 4, 9]
+//        c << [7, 5, 9]
+//    }
+//}
