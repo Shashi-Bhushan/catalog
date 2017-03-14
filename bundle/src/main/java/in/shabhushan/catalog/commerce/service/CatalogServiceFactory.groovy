8faef353c358825be20dd2a0a5adbbbeb6c0d8db
Shashi Bhushan <sbhushan1@outlook.com>
2017-03-14 14:29:12 +0530

    Service Factory Properties added

--- bundle/src/main/java/in/shabhushan/catalog/commerce/service/CatalogServiceFactory.groovy
+++ bundle/src/main/java/in/shabhushan/catalog/commerce/service/CatalogServiceFactory.groovy
@@ -19,7 +19,6 @@ package in.shabhushan.catalog.commerce.service;
 import com.adobe.cq.commerce.api.CommerceService;
 import com.adobe.cq.commerce.api.CommerceServiceFactory;
 import com.adobe.cq.commerce.common.AbstractJcrCommerceServiceFactory
-import groovy.util.logging.Slf4j;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
@@ -32,8 +31,10 @@ import org.apache.sling.api.resource.Resource;
  */
 @Component(label = "Catalog Service Factory", description = "Factory for reference implementation commerce service", metatype = true)
 @Service(CommerceServiceFactory.class)
-@Property(name = "commerceProvider", value = "catalog", propertyPrivate = true)
-public class CatalogServiceFactory extends AbstractJcrCommerceServiceFactory implements CommerceServiceFactory {
+@Properties([
+    @Property(name = "commerceProvider", value = "catalog", propertyPrivate = true)
+])
+class CatalogServiceFactory extends AbstractJcrCommerceServiceFactory implements CommerceServiceFactory {
     @Override
     public CommerceService getCommerceService(Resource resource) {
         return new CatalogServiceImpl(getServiceContext(), resource);
