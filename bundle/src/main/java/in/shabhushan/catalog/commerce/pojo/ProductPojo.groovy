a076fd5018a91476c4bba96697ba8a58572eeeeb
Shashi Bhushan <sbhushan1@outlook.com>
2017-03-29 20:42:53 +0530

    Catalog Product Pojo Working

--- bundle/src/main/java/in/shabhushan/catalog/commerce/pojo/ProductPojo.groovy
+++ bundle/src/main/java/in/shabhushan/catalog/commerce/pojo/ProductPojo.groovy
@@ -1,6 +1,12 @@
 package in.shabhushan.catalog.commerce.pojo
 
+import com.adobe.cq.commerce.api.CommerceService
+import com.adobe.cq.commerce.api.CommerceSession
+import com.adobe.cq.commerce.api.Product
 import com.adobe.cq.sightly.WCMUsePojo
+import org.apache.sling.api.SlingHttpServletRequest
+import org.apache.sling.api.SlingHttpServletResponse
+import org.apache.sling.api.resource.Resource
 
 /**
  * Created by Shashi Bhushan
@@ -8,10 +14,25 @@ import com.adobe.cq.sightly.WCMUsePojo
  */
 class ProductPojo extends WCMUsePojo{
 
-    private String productData;
+    String productData
+
+    Product product
 
     @Override
     void activate() throws Exception {
-        productData = getProperties().get("productData", "")
+        productData = getProperties().get("productData", String)
+
+        CommerceService commerceService = getResource().adaptTo(CommerceService.class)
+        commerceService.login(getRequest(), getResponse())
+
+        product = commerceService.getProduct(productData)
+    }
+
+    String getTitle() {
+        product.getProperty("jcr:title", String)
+    }
+
+    def getRating() {
+        product.getProperty("catalogRating", String)
     }
 }
