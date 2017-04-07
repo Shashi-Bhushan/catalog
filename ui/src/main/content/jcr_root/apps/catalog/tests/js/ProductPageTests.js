85fe3a5df2d29b82e4ffcb71eb38e9a23a4987e1
Shashi Bhushan <sbhushan1@outlook.com>
2017-04-07 19:49:44 +0530

    Hobbes test refactoring

--- ui/src/main/content/jcr_root/apps/catalog/tests/js/ProductPageTests.js
+++ ui/src/main/content/jcr_root/apps/catalog/tests/js/ProductPageTests.js
@@ -1,25 +1,35 @@
-new hobs.TestSuite("Catalog - Product Page Tests", {
-        path:"/apps/catalog/tests/js/ProductPageTests.js",
-        register: true
-    })
-    .addTestCase(
+"use strict";
+
+(function(hobs){
+
+    var productPageHref = "/content/catalog/en/fiction/the-best-we-could-do.html";
+
+    var hobsTestSuite = new hobs.TestSuite("Catalog - Product Page Tests", {
+            path:"/apps/catalog/tests/js/ProductPageTests.js",
+            register: true
+        });
+
+    hobsTestSuite.addTestCase(
         new hobs.TestCase("Navigate To Catalog Product")
-            .navigateTo("/content/catalog/en/fiction/the-best-we-could-do.html")
-            .asserts.location("/content/catalog/en/fiction/the-best-we-could-do.html", true)
-    )
-    .addTestCase(
+            .navigateTo(productPageHref)
+            .asserts.location(productPageHref, true)
+    );
+
+    hobsTestSuite.addTestCase(
         new hobs.TestCase("Open Model")
-            .navigateTo("/content/catalog/en/fiction/the-best-we-could-do.html")
+            .navigateTo(productPageHref)
             .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade")
             .click("button[data-target='#myModal']")
             .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade in")
-    )
-    .addTestCase(
+    );
+
+    hobsTestSuite.addTestCase(
         new hobs.TestCase("Open Model and Close")
-            .navigateTo("/content/catalog/en/fiction/the-best-we-could-do.html")
+            .navigateTo(productPageHref)
             .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade")
             .click("button[data-target='#myModal']")
             .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade in")
             .click("button[class='close'][data-dismiss='modal']")
             .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade")
     )
+})(hobs);
