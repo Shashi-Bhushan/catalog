819e874eb1ac38e7555957926c56be04d6bd4622
Shashi Bhushan <sbhushan1@outlook.com>
2017-04-07 16:16:53 +0530

    Hobbed Test case for Product Page

---
+++ ui/src/main/content/jcr_root/apps/catalog/tests/js/ProductPageTests.js
@@ -0,0 +1,16 @@
+new hobs.TestSuite("Catalog - Product Page Tests", {
+        path:"/apps/catalog/tests/js/ProductPageTests.js",
+        register: true
+    })
+    .addTestCase(
+        new hobs.TestCase("Navigate To Catalog Product")
+            .navigateTo("/content/catalog/en/fiction/the-best-we-could-do.html")
+            .asserts.location("/content/catalog/en/fiction/the-best-we-could-do.html", true)
+    )
+    .addTestCase(
+        new hobs.TestCase("Open Model")
+            .navigateTo("/content/catalog/en/fiction/the-best-we-could-do.html")
+            .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade")
+            .click("button[data-target='#myModal']")
+            .asserts.hasCssClass("div[id='myModal']:has(div[class='modal-content'])", "modal fade in")
+    )
