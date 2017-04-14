be38a47e8e78172a668ad74f814d501eaf122ba4
Shashi Bhushan <sbhushan1@outlook.com>
2017-04-14 18:19:08 +0530

    Login Component Added

--- ui/src/main/content/jcr_root/apps/catalog/components/content/login/sign-in-wrapper/clientlib/js/signup.js
+++ ui/src/main/content/jcr_root/apps/catalog/components/content/login/sign-in-wrapper/clientlib/js/signup.js
@@ -1,10 +1,33 @@
 "use strict";
 
 (function($){
+    $.fn.initiateLogin = function(opts) {
+        var $loginForm = this;
+
+        options = $.extend({
+            path : '/content/catalog/en',
+            securityToken: 'j_security_check',
+        }, opts);
+
+        console.log("options are : " + options);
+        $loginForm.submit(function(event){
+
+            $.ajax({
+                url: options.path + '/j_security_check',
+                method: 'post',
+                data: $loginForm.serialize(),
+                success: function(response){
+                    console.log(response);
+
+                    window.location.reload();
+                }
+            });
+            return false;
+        });
+    };
+
     $(document).ready(function(){
         $('#signup input[type="submit"]').on('click', function(event){
-            event.preventDefault();
-
             var $signupModal = $('#loginModal #signup form[name="loginForm"]');
 
 //             TODO : Show error on password and confirm password mismatch
@@ -22,6 +45,11 @@
                     console.log(response)
                 }
             });
+
+            // Equivalent of event.preventDefault();
+            return false;
         });
+
+
     });
 })(jQuery);
