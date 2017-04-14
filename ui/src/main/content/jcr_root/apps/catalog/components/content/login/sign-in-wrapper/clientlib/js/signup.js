222fa276b757f49730c894f319707692bb1236ef
Shashi Bhushan <sbhushan1@outlook.com>
2017-04-14 16:25:41 +0530

    Signup.js change

--- ui/src/main/content/jcr_root/apps/catalog/components/content/login/sign-in-wrapper/clientlib/js/signup.js
+++ ui/src/main/content/jcr_root/apps/catalog/components/content/login/sign-in-wrapper/clientlib/js/signup.js
@@ -5,18 +5,13 @@
         $('#signup input[type="submit"]').on('click', function(event){
             event.preventDefault();
 
-            var $signupModal = $('#loginModal #signup');
+            var $signupModal = $('#loginModal #signup form[name="loginForm"]');
 
 //             TODO : Show error on password and confirm password mismatch
             $.ajax({
                 url: '/bin/signup',
                 method: 'post',
-                data: {
-                    'j_username' : $signupModal.find('input[name="j_username"]').val(),
-                    'j_password' : $signupModal.find('input[name="j_password"]').val(),
-                    'j_name' : $signupModal.find('input[name="j_name"]').val(),
-                    'j_email' : $signupModal.find('input[name="j_email"]').val()
-                },
+                data: $signupModal.serialize(),
                 success: function(response) {
                     console.log(response);
                     if(response.status) {
