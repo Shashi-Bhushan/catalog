708691a3a6f836ff239d997470ca11b4dc06bca0
Shashi Bhushan <sbhushan1@outlook.com>
2018-08-18 09:16:12 +0530

    Groovy File Redundant ; removed

--- core/src/main/java/in/shabhushan/catalog/commerce/framework/provider/session/CatalogSessionImpl.groovy
+++ core/src/main/java/in/shabhushan/catalog/commerce/framework/provider/session/CatalogSessionImpl.groovy
@@ -16,25 +16,25 @@
 
 package in.shabhushan.catalog.commerce.framework.provider.session;
 
-import com.adobe.cq.commerce.api.CommerceException;
-import com.adobe.cq.commerce.common.AbstractJcrCommerceService;
-import com.adobe.cq.commerce.common.AbstractJcrCommerceSession;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.adobe.cq.commerce.api.CommerceException
+import com.adobe.cq.commerce.common.AbstractJcrCommerceService
+import com.adobe.cq.commerce.common.AbstractJcrCommerceSession
+import org.apache.sling.api.SlingHttpServletRequest
+import org.apache.sling.api.SlingHttpServletResponse
+import org.apache.sling.api.resource.Resource
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Created by Shashi Bhushan
  *      on 13/3/17.
  */
-public class CatalogSessionImpl extends AbstractJcrCommerceSession {
+class CatalogSessionImpl extends AbstractJcrCommerceSession {
 
     private static final Logger LOG = LoggerFactory.getLogger(CatalogSessionImpl.class);
 
-    public CatalogSessionImpl(AbstractJcrCommerceService commerceService, SlingHttpServletRequest request,
+    CatalogSessionImpl(AbstractJcrCommerceService commerceService, SlingHttpServletRequest request,
                               SlingHttpServletResponse response, Resource resource) throws CommerceException {
-        super(commerceService, request, response, resource);
+        super(commerceService, request, response, resource)
     }
 }
