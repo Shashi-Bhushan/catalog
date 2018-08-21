7c7b31a6b007d574b2e8f3da01a134760f3e7550
Shashi Bhushan <sbhushan1@outlook.com>
2018-08-21 15:17:10 +0530

    Signup and Login Modal Changes

---
+++ core/src/main/java/in/shabhushan/catalog/bindings/CatalogBindings.groovy
@@ -0,0 +1,21 @@
+package in.shabhushan.catalog.bindings
+
+import in.shabhushan.catalog.constants.ToolTipPlacement
+import org.apache.felix.scr.annotations.Component
+import org.apache.felix.scr.annotations.Service
+import org.apache.sling.scripting.api.BindingsValuesProvider
+
+import javax.script.Bindings
+
+/**
+ * @author Shashi Bhushan
+ */
+@Component
+@Service
+class CatalogBindings implements BindingsValuesProvider{
+
+    @Override
+    void addBindings(Bindings bindings) {
+        bindings.put("tooltipPlacement", ToolTipPlacement.BOTTOM.toolTipPlacement)
+    }
+}
